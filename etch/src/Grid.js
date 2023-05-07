a493e1f26645c80e9915311fcba5075d2aed6bbd
Max Abimbola <max.abimbola@hotmail.com>
2023-05-07 20:56:45 +0100

    dfsdfdsf

--- etch/src/Grid.js
+++ etch/src/Grid.js
@@ -1,17 +1,30 @@
 import React,{Component} from 'react';
 import Box from './Box';
 import './Grid.css';
-import ColourWheel from './ColourWheel';
-
-const size = 10
+import ChromePicker from './ColourWheel';
 
 class Grid extends Component{
+    static defaultProps = {
+        size: 10
+    }
     constructor(props){
         super(props);
         this.state = {
-            gridArray : this.generateGridArray(size),
+            gridArray : [[0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0]],
             brushColour : '#000000',
             mouseDown: false,
+            size: 10,
+            displayColourPicker: false
             
         }
         this.changeColour = this.changeColour.bind(this)
@@ -20,6 +33,9 @@ class Grid extends Component{
         this.handleOnMouseUp = this.handleOnMouseUp.bind(this)
         this.handleMouseOver = this.handleMouseOver.bind(this)
         this.changeGridSize = this.changeGridSize.bind(this)
+        this.handleClick = this.handleClick.bind(this)
+        this.clearScreen = this.clearScreen.bind(this)
+        this.handClickColorButtton = this.handClickColorButtton.bind(this)
     }
 
     
@@ -37,6 +53,10 @@ class Grid extends Component{
         this.changeColour(e.target.id)
 
         
+    }
+
+    handClickColorButtton(){
+        this.setState({displayColourPicker: !this.state.displayColourPicker})
     }
 
     handleOnMouseUp(e){
@@ -72,13 +92,35 @@ class Grid extends Component{
 
     }
 
+    handleClick(evt){
+        this.clearScreen()
+        const newGrid = this.generateGridArray(evt.target.id)
+        this.setState({gridArray: newGrid, size:evt.target.id})
+    }
+
+    clearScreen(){
+        const boxes = document.querySelectorAll('.box')
+        boxes.forEach((x)=>{
+            x.style='#FFFFFF'
+        })
+
+    }
+
 
     render(){
+        const cover={
+            position: 'fixed',
+            top: '0px',
+            right: '1000px',
+            bottom: '0px',
+            left: '100px'
+        }
 
         const gridRender = this.state.gridArray.map((boxRow,row)=>boxRow.map((box,col)=>(
             <Box
                 key={`${row}-${col}`}
                 id={`${row}-${col}`}
+                className='box'
                 changeColour={this.changeColour}
                 handleMouseDown={this.handleMouseDown}
                 handleMouseOver={this.handleMouseOver}
@@ -87,26 +129,36 @@ class Grid extends Component{
         )))
         const gridTemplateStyles = {
             display:'grid',
-            gridTemplateRows:`repeat(${size},1fr)`,
-            gridTemplateColumns:`repeat(${size},1fr)`
+            gridTemplateRows:`repeat(${this.state.size},1fr)`,
+            gridTemplateColumns:`repeat(${this.state.size},1fr)`
             
         }
         return(
             <div className='container'>
-                <div className='controls'>
-                    <div className='colourWheel'>
-                        <ColourWheel
-                        setPenColour={this.setPenColour}/>
-                    </div>
-                    <div id='grid-size-boxes-container'>
-                        <button>2x2</button>
-                        <button>5x5</button>
-                        <button>10x10</button>
-                        <button>20x20</button>
-                        <button>50x50</button>
-                    </div>
+                
+
+                <div className='settings'>
+                        <button style={{backgroundColor:this.state.brushColour}} className='colour-picker-button' onClick={this.handClickColorButtton}></button>
+                        {this.state.displayColourPicker ? 
+                            <div className='colourWheel-container' style={cover}>
+                                <ChromePicker
+                                className='colourWheel'
+                                setPenColour={this.setPenColour}/>
+                                
+                            </div> :null}
+                        <div className='size-buttons-container'>
+                            <input name='2-button' type='button' value='2X2' id='2'  onClick={this.handleClick}/>
+                            <input name='5-button' type='button' value='5X5' id='5'  onClick={this.handleClick}/>
+                            <input name='10-button' type='button' value='10X10' id='10'  onClick={this.handleClick}/>
+                            <input name='20-button' type='button' value='20X20' id='20'  onClick={this.handleClick}/>
+                        </div>
+                        <div id='cls-button-container'>
+                            <button id='cls-button' onClick={this.clearScreen} >CLEAR</button>
+                        </div>
                 </div>
+               
                 <div className='gridContainer' style={gridTemplateStyles} >
+            
                     {gridRender}
                 </div>
 
